07b8c92ace4f4b71935543f2e70492c444e9b5a9
Ahmad Malik <arma7x@live.com>
2022-07-06 13:10:30 +0800

    fix external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,10 @@ export default {
     sourcemap: !production,
     format: 'iife',
     name: 'app',
-    file: 'public/build/bundle.js'
+    file: 'public/build/bundle.js',
+    globals: {
+      QRCode: 'window.QRCode'
+    }
   },
   external: ['QRCode'],
   context: 'window',
