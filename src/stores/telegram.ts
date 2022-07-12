a4046e7afc0fa8b4fd9ac32dfbf971e74ff64306
Ahmad Malik <arma7x@live.com>
2022-07-12 15:56:36 +0800

    WIP: telegram store

--- src/stores/telegram.ts
+++ src/stores/telegram.ts
@@ -1,6 +1,8 @@
 import { get, writable } from 'svelte/store';
 import { TelegramKeyHash, Api, client } from '../utils/mtproto_client';
 
+export const connectionStatus = writable(false);
+export const authorizedStatus = writable(false);
 export const chatCollections = writable([]);
 
 export async function retrieveChats() {
