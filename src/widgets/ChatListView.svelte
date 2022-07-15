7f82c542b8650859b58ead69f12260a7c2f38eec
Ahmad Malik <arma7x@live.com>
2022-07-15 23:01:02 +0800

    fallback to username

--- src/widgets/ChatListView.svelte
+++ src/widgets/ChatListView.svelte
@@ -12,7 +12,7 @@
     <p>
       {chat.name}
     </p>
-    <small>{#if chat.isGroup}<b>{chat.message._sender.username}</b>: {/if}{chat.message.message.substring(0, (chat.isGroup ? 15 : 30)) + (chat.message.message.length > (chat.isGroup ? 15 : 30) ? '...' : '')}</small>
+    <small>{#if chat.isGroup}<b>{chat.message._sender.username || chat.message._sender.firstName}</b>: {/if}{chat.message.message.substring(0, (chat.isGroup ? 15 : 30)) + (chat.message.message.length > (chat.isGroup ? 15 : 30) ? '...' : '')}</small>
   </div>
   <div class="kai-list-view-indicator">
     {#if chat.unreadCount}
