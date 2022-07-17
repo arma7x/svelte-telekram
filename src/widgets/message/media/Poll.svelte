e3c1b534318719cb17920723d2cd66389a28bd94
Ahmad Malik <arma7x@live.com>
2022-07-18 00:49:50 +0800

    fix poll status

--- src/widgets/message/media/Poll.svelte
+++ src/widgets/message/media/Poll.svelte
@@ -3,13 +3,23 @@
 
   export let message: any = {};
 
+  let available: bool = true;
+  let answeredOrVoted: bool = false;
+
   onMount(() => {
     console.log('--------------------------------');
+    console.log(message);
     console.log(message.media.poll.question, `quiz: ${message.media.poll.quiz}`, `multipleChoice: ${message.media.poll.multipleChoice}`, message.media.poll.answers);
-    if (message.media.results) {
-      console.log(message.media.results.results);
-      if (message.media.results.solution)
-        console.log(message.media.results.solution);
+    if (message.media.results.solution)
+      console.log(message.media.results.solution);
+    if (message.media.poll.closed)
+      available = false;
+    for (let r in message.media.results.results) {
+      if (message.media.results.results[r].chosen) {
+        available = false;
+        answeredOrVoted = true;
+        break;
+      }
     }
   })
 
@@ -19,8 +29,8 @@
 <div class="media-container">
   <b>{message.media.poll.quiz ? 'Quiz' : 'Poll'}:</b>
   <p>{message.media.poll.question}</p>
-  {#if message.media.results}
-  <small><i>Status: {message.media.poll.quiz ? 'Answered' : 'Voted'}</i></small>
+  {#if !available}
+  <small><i>Status: {answeredOrVoted ? (message.media.poll.quiz ? 'Answered' : 'Voted') : 'Ended'}</i></small>
   {:else}
   <small><i>Status: Available</i></small>
   {/if}
