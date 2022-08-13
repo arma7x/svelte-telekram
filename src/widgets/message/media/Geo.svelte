bcaf724b71311e7efeb30f5b1ee707b42cd4d749
Ahmad Malik <arma7x@live.com>
2022-08-13 14:39:07 +0800

    add GEo action menu

--- src/widgets/message/media/Geo.svelte
+++ src/widgets/message/media/Geo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy, beforeUpdate } from 'svelte';
   import { createKaiNavigator, KaiNavigator } from '../../../utils/navigation';
+  import { OptionMenu } from '../../../components';
 
   export let chat: any = {};
   export let message: any = {};
@@ -8,15 +9,83 @@
   export let registerCallButtonHandler: Function = (id, callback) => {}
   export let refetchMessage: Function = (id: number) => {}
 
-  function actionMenu() {
-    window.open(`https://www.google.com/maps/search/?api=1&query=${message.media.geo.lat},${message.media.geo.long}`, '_blank').focus();
-  }
+  let menu: OptionMenu;
 
   let url: string = '';
 
+  // https://help.openstreetmap.org/questions/20335/embedded-html-displays-zoomed-out/51323
+  function getCoordOffset($what, $lat, $lon, $offset) {
+    var $earthRadius = 6378137;
+    var $coord = [$lat, $lon];
+    var $radOff = $what === 0 ? $offset / $earthRadius : $offset / ($earthRadius * Math.cos(Math.PI * $coord[0] / 180));
+    return $coord[$what] + $radOff * 180 / Math.PI;
+  }
+
+  function getBBox($lat, $lon, $area) {
+    var $offset = $area / 2;
+    // 0 = minlon, 1 = minlat, 2 = maxlon, 3 = maxlat, 4,5 = original val (marker)
+    return [
+      getCoordOffset(1, $lat, $lon, -$offset),
+      getCoordOffset(0, $lat, $lon, -$offset),
+      getCoordOffset(1, $lat, $lon, $offset),
+      getCoordOffset(0, $lat, $lon, $offset),
+      $lat,
+      $lon
+    ];
+  }
+
+  function getOpenStreetMapEmbed(lat, long) {
+    var result = getBBox(lat, long, 10);
+    return `https://www.openstreetmap.org/export/embed.html?bbox=${result[0]},${result[1]},${result[2]},${result[3]}&layer=mapnik&marker=${lat},${long}`;
+  }
+
+  function getGoogleMapEmbed(lat, long) {
+    return `https://maps.google.com/maps?width=100px;height=100px&hl=en&q=${lat},${long}+(Location)&t=&z=15&ie=UTF8&iwloc=B&output=embed`
+  }
+
+  function actionMenu() {
+    setTimeout(() => {
+      menu = new OptionMenu({
+        target: document.body,
+        props: {
+          title: 'Action Menu',
+          focusIndex: 0,
+          options: [{ title: 'Open in Google Map' }, { title: 'Open in OpenStreetMap' }],
+          softKeyCenterText: 'select',
+          onSoftkeyRight: (evt, scope) => {},
+          onSoftkeyLeft: (evt, scope) => {},
+          onEnter: (evt, scope) => {
+            menu.$destroy();
+            if (scope.selected.title === 'Open in Google Map') {
+              window.open(`https://www.google.com/maps/search/?api=1&query=${message.media.geo.lat},${message.media.geo.long}`, '_blank').focus();
+            } else if (scope.selected.title === 'Open in OpenStreetMap') {
+              const url = getOpenStreetMapEmbed(message.media.geo.lat, message.media.geo.long);
+              window.open(url, '_blank').focus();
+            }
+          },
+          onBackspace: (evt, scope) => {
+            evt.preventDefault();
+            evt.stopPropagation();
+            menu.$destroy();
+          },
+          onOpened: () => {
+            parentNavInstance.detachListener();
+          },
+          onClosed: (scope) => {
+            parentNavInstance.attachListener();
+            menu = null;
+          }
+        }
+      });
+    }, 100);
+  }
+
+  beforeUpdate(() => {
+    url = getGoogleMapEmbed(message.media.geo.lat, message.media.geo.long);
+  });
+
   onMount(() => {
     registerCallButtonHandler(message.id.toString(), actionMenu);
-    url = `https://maps.google.com/maps?width=100px;height=100px&hl=en&q=${message.media.geo.lat},${message.media.geo.long}+(Location)&t=&z=15&ie=UTF8&iwloc=B&output=embed`
   })
 
 </script>
