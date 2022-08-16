c0313ace39e01379f1775b6fcfd44ae7968c796c
Ahmad Malik <arma7x@live.com>
2022-08-16 15:49:35 +0800

    fix styles

--- src/routes/Room.svelte
+++ src/routes/Room.svelte
@@ -1001,7 +1001,7 @@
 
 <svelte:options accessors immutable={true}/>
 
-<main id="room-screen" data-pad-top="{padTop ? '38' : '0'}" data-pad-bottom="40">
+<main id="room-screen" data-pad-top="{padTop ? '50' : '0'}" data-pad-bottom="30">
   {#if ready }
   {#each messages as message}
     {#if message && message.id && messageMetadata[message.id.toString()] && messageMetadata[message.id.toString()].deleted === false}
