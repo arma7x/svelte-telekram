22bbfecc9f53ca8d9e8152222f75c0393cf19200
Ahmad Malik <arma7x@live.com>
2022-08-21 00:32:15 +0800

    polyfill String.prototype.replaceAll

--- src/utils/bootstrap.ts
+++ src/utils/bootstrap.ts
@@ -1,6 +1,17 @@
 declare var telegram:any;
 declare var idb:any;
 
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function(str, newStr) {
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr);
+    }
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+  };
+}
+
 import { parseUA } from './misc';
 import TelegramKeyHash from '../telegram_key';
 
